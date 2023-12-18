2a0850d3c4d3b15aa0a35525081e17b8356d40d0
ErbolKarmyshakov <erbolkarmysakov37@gmail.com>
2023-12-17 21:54:44 -0600

    Initial Commit

---
+++ NotesMobileApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesMobileApp
+//
+//  Created by Mac User on 17/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
